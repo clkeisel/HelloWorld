9b896dca5dbd8e97f468a6f944ced14a29213ec0
Clay <clkeisel@yahoo.com>
2015-02-02 16:10:26 -0700

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Clay Keisel on 2/2/15.
+//  Copyright (c) 2015 Clay Keisel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
